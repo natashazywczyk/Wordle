5f977cdb2b2c3856021cdbbf04b589fbe0addaf2
NATASHA ZYWCZYK <G00415696@atu.ie>
2023-12-29 23:58:17 +0000

    added xmlns model

---
+++ INotifyPropertyChanged.cs
@@ -0,0 +1,6 @@
+﻿namespace Wordle
+{
+    public interface INotifyPropertyChanged
+    {
+    }
+}
---
+++ WordleViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wordle;
+public class WordleViewModel : INotifyPropertyChanged 
+{ 
+
+}
+
+public interface INotifyPropertyChanged
+{
+}
