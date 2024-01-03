6d2039f91c629f414488e97efb4ff1997d69b107
NATASHA ZYWCZYK <G00415696@atu.ie>
2024-01-03 11:26:24 +0000

    added .cs for storing letter variables

---
+++ RowLetters.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wordle
+{
+    internal class RowLetters
+    {
+        public string FirstLetter { get; set; };
+        public string SecondLetter { get; set; };
+        public string ThirdLetter { get; set; };
+        public string ForthLetter { get; set; };
+        public string LastLetter { get; set; };
+    }
+}
