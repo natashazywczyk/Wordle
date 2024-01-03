ae76f7855f507679f0e074ac268442cc462d4400
NATASHA ZYWCZYK <G00415696@atu.ie>
2024-01-03 15:54:33 +0000

    added methods for changing colours for correc t space and incorrect space

--- Settings.cs
+++ Settings.cs
@@ -13,6 +13,40 @@ namespace Wordle
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
+        private string correctSpace;
+        private string incorrectSpace;
+
+        public string CorrectSpace
+        {
+            get => correctSpace;
+            set
+            {
+                if (correctSpace != value)
+                {
+                    correctSpace = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public string IncorrectSpace
+        {
+            get => incorrectSpace;
+            set
+            {
+                if (incorrectSpace != value)
+                {
+                    incorrectSpace = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        //Constructor
+        public Settings()
+        {
+            CorrectSpace = "#2B0B98";
+            IncorrectSpace = "#2B0B98";
+        }
 
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
