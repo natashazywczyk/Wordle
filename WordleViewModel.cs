918ffefbcba85acac545d1ec2c7d43e13a890bb0
NATASHA ZYWCZYK <G00415696@atu.ie>
2024-01-03 12:02:39 +0000

    added OnPropertyCHanged method

--- WordleViewModel.cs
+++ WordleViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,6 +13,10 @@ public class WordleViewModel : INotifyPropertyChanged
 
     WordleViewModel wordleModel;
 
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
 
     public void check(char[] answer)
     {
