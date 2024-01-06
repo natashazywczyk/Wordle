3578a8c5c8442a2b68f6ad2d7491465acefe1c5c
NATASHA ZYWCZYK <G00415696@atu.ie>
2024-01-06 17:27:49 +0000

    started to make the program read from file

--- ReadWordList.cs
+++ ReadWordList.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace Wordle
 {
-    internal class ReadWordList
+    public class ReadWordList
     {
+        Random rand;
+
+        private List<string> wordsFromList;
+        private int length;
+
+        public ReadWordList(Random rand)
+        {
+            this.rand = rand;
+        }
+
+        public List<string> WordsFromList
+        {
+            get { return wordsFromList; }  
+            set { wordsFromList = value; }
+        }
+
+        public int Length
+        {
+            get { return length; }
+            set { length = value; }
+        }
     }
 }
