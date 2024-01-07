d52626c45405575a9a6ff02b000fb056b0d0e440
NATASHA ZYWCZYK <G00415696@atu.ie>
2024-01-07 11:41:02 +0000

    added more text

--- RulesPage.xaml.cs
+++ RulesPage.xaml.cs
@@ -10,7 +10,15 @@ public partial class RulesPage : ContentPage
 
 		var Info = new Label
 		{
-			Text = "Welcome to Wordle!",
+			Text = "Welcome to Wordle!" +
+			"\n\nIn this game, your goal to to guess a random 5 digit word, from a bank of 3000 words" +
+			"\n\nYou are given 6 chances to guess one word per round" +
+			"\nIf you fail to guess it correctly in 6 turns, you lose!" +
+			"\n\nIf you have a letter that's included in the word, in the correct space, the box will turn green" +
+			"\nIf you guess a letter that's included in the word, but in the incorrect space, the box will turn yellow" +
+			"\nIncorrect answers will remain the same colour as before submitting your guess" +
+			"\n\nYou have 60 seconds to guess the correct word" +
+			"\n\n\nGood Luck!!!",
             HorizontalTextAlignment = TextAlignment.Center,
             VerticalTextAlignment = TextAlignment.Center,
             FontSize = 20
