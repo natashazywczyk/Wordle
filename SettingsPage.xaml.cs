1e93f51f86f6ec9d84ed868c52839c6ccb05879e
NATASHA ZYWCZYK <G00415696@atu.ie>
2024-01-08 21:24:12 +0000

    comment

--- SettingsPage.xaml.cs
+++ SettingsPage.xaml.cs
@@ -10,6 +10,7 @@ public partial class SettingsPage : ContentPage
         BindingContext = set;
     }
 
+    //Use SaveJson method when saved btn is clicked to save the new settings as a .json file and go back to MainPage
     private async void SaveBtn_Clicked(object sender, EventArgs e)
     {
         set.SaveJson();
